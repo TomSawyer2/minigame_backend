5227df60396e6eec6f86a10e6269d6dd69373416
TomSawyer2 <6272867@qq.com>
2022-02-13 13:00:47 +0800

    feat(rank)：排行榜支持自定义数量

--- src/controller/game2.js
+++ src/controller/game2.js
@@ -9,23 +9,32 @@ const apis = {
     updateInfo(req, res, next) {
         // 数据库更新数据示例
         const data = req.body;
-        let newRecord = new gameTwo({
-            username: data.username,
-            phone: data.phone,
-            score: data.score
-        })
-        newRecord.save((err) => {
-            if (err) next({ status: errorcode.UPDATE_ERR_2, msg: '更新成绩错误' })
-            res.send({ status: 0, msg: '操作成功', data: null });
-        })
+        if (data.score <= 0) {
+            next({ status: errorcode.UPDATE_ERR_2, msg: '更新成绩错误' })
+        } else {
+            let newRecord = new gameTwo({
+                username: data.username,
+                phone: data.phone,
+                score: data.score
+            })
+            newRecord.save((err) => {
+                if (err) next({ status: errorcode.UPDATE_ERR_2, msg: '更新成绩错误' })
+                res.send({ status: 0, msg: '操作成功', data: null });
+            })
+        }
     },
 
     getrank(req, res, next) {
-        // 数据库查找前十大数据示例
-        gameTwo.find().sort({ score: -1 }).skip(0).limit(10).exec((err, response) => {
-            if (err) next({ status: errorcode.GET_RANK_ERR_2, msg: '查找排行榜错误' })
-            res.send({ status: 0, msg: '操作成功', data: response })
-        })
+        const data = req.query;
+        if (data.number <= 0) {
+            res.send({ status: 0, msg: '操作成功', data: null })
+        } else {
+            // 数据库查找前{number}大数据示例
+            gameTwo.find().sort({ score: -1 }).skip(0).limit(data.number).exec((err, response) => {
+                if (err) next({ status: errorcode.GET_RANK_ERR_2, msg: '查找排行榜错误' })
+                res.send({ status: 0, msg: '操作成功', data: response })
+            })
+        }
     }
 }
 
